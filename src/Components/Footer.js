b6786eec677c74e009c4b5e1c7836d15865ffe55
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-09-22 12:39:58 -0300

    Update footer links and add WhatsApp contact for ordering lettuce

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,33 +10,30 @@ const Footer = () => {
     <div className="footer-wrapper">
       <div className="footer-section-one">
         <div className="footer-logo-container">
-          <img src={Logo} alt="" />
+          <img src={Logo} alt="logo" />
         </div>
         <div className="footer-icons">
-          <BsTwitter />
-          <SiLinkedin />
-          <BsYoutube />
-          <FaFacebookF />
+          <a href=""><BsYoutube /></a>
+          <a href="https://www.facebook.com/groups/manutencaoextintores/"><FaFacebookF /></a>
+          <a href="https://www.linkedin.com/in/ligia-andrade-9b9a7120/"><SiLinkedin /></a>
         </div>
       </div>
       <div className="footer-section-two">
         <div className="footer-section-columns">
-          <span>Qualtiy</span>
-          <span>Help</span>
-          <span>Share</span>
-          <span>Carrers</span>
-          <span>Testimonials</span>
+          <span>Qualidade</span>
+          <span>Ajuda</span>
+          <span>Compartilhar</span>
           <span>Work</span>
         </div>
         <div className="footer-section-columns">
-          <span>244-5333-7783</span>
-          <span>hello@food.com</span>
-          <span>press@food.com</span>
-          <span>contact@food.com</span>
+          <a href="https://wa.me/5511972694511">11972694511</a>
+          <span>contato.naturalmentebom@gmail.com</span>
+          <span>ligia.amsilva@gmail.com</span>
+          <span>thiago.sandrade0720@gmail.com</span>
         </div>
         <div className="footer-section-columns">
           <span>Terms & Conditions</span>
-          <span>Privacy Policy</span>
+          <span>Feito com ❤ por <a href="https://www.linkedin.com/in/thiagoandrade072/">Thiago Silva Andrade</a></span>
         </div>
       </div>
     </div>
